330cf857bd3e16e4059dd06a69ac441f898302ec
Arne Tripolone <arnetripolone@gmail.com>
2021-10-16 19:26:55 +1100

    github pages operational! Started state for navbar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Arne Tripolone',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About Me', path: '/about' },
+        { title: 'Work Examples', path: '/work' },
+        { title: 'Contact Me', path: '/contact' }
+      ]
+    }
+  }
+  
+  render() {
+    return (
+      <div>Hello from React</div>
+    )
+  }
 }
 
 export default App;
