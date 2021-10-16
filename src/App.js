f9b2624c585fd2d39e203c281b7e6e22f0eb70d9
Arne Tripolone <arnetripolone@gmail.com>
2021-10-16 20:18:44 +1100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Navbar';
+
 import './App.css';
+
 
 class App extends React.Component {
 
@@ -36,7 +40,22 @@ class App extends React.Component {
     return (
       <Router>
         <Container fluid={true}>
-          <p>Hi from React!</p>
+
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
+            <Navbar.Brand>Arne Tripolone</Navbar.Brand>
+
+            <Navbar.Toggle className='border-0' aria-controls='navbar-toggle' />
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className='ml-auto'>
+                <Link className='nav-link' to='/'>Home</Link>
+                <Link className='nav-link' to='/about'>About Me</Link>
+                <Link className='nav-link' to='/work'>Work Examples</Link>
+                <Link className='nav-link' to='/contact'>Contact Me</Link>
+              </Nav>
+            </Navbar.Collapse> 
+
+          </Navbar>
+
         </Container>
       </Router>
     )
